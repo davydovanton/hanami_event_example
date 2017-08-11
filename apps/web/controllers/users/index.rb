2753f4cd840540c59742c2ae4deea6404b79079d
Anton Davydov <antondavydov.o@gmail.com>
2017-08-11 03:02:03 +0300

    Display only page specific events

--- apps/web/controllers/users/index.rb
+++ apps/web/controllers/users/index.rb
@@ -4,8 +4,8 @@ module Web::Controllers::Users
     expose :users, :events
 
     def call(params)
-      @events = REDIS.with { |conn| conn.lrange('hanami.event_store', 0, -1) }
-      @events = @events.map { |json| JSON.parse(json) }
+      @events = REDIS.with { |conn| conn.lrange('hanami.event_store', 0, -1) }.map! { |json| JSON.parse(json) }
+      @events = @events.select { |e| e['event_name'][/created/] }
       @users = UserRepository.new.users.order { created_at.desc }.to_a
     end
   end
